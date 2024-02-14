9478b0c5a5b02a47c0d6276d5a56c2218a199361
mac <mac@Aidanas-mac.local>
2024-02-14 15:07:37 +0600

    chanh

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import HomePage from '../pages/HomePage'
+import ProductPage from '../pages/ProductPage'
+import NotFoundPage from '../pages/NotFoundPage'
+import CartPage from '../pages/CartPage'
+import AuthPage from '../pages/AuthPage'
+import AdminPage from '../pages/AdminPage'
+import EditPage from '../pages/EditPage'
+import { Route, Routes } from 'react-router-dom'
+
+const MainRoutes = () => {
+    const PUBLIC_ROUTES=[
+        { id: 1, link: "/", element: <HomePage /> },
+        { id: 2, link: "/products", element: <ProductPage /> },
+        { id: 3, link: "*", element: <NotFoundPage /> },
+        { id: 4, link: "/cart", element: <CartPage/> },
+        { id: 5, link: "/auth", element: <AuthPage /> },
+        { id: 6, link: "/admin", element: <AdminPage/> },
+        { id: 7, link: "/edit/:id", element: <EditPage/> },
+    ]
+  return (
+    <Routes>
+        {PUBLI.C_ROUTES.map((elem) => (
+          <Route path={elem.link} key={elem.id} element={elem.element} />
+        ))}
+    </Routes>
+  )
+}
+
+export default MainRoutes
